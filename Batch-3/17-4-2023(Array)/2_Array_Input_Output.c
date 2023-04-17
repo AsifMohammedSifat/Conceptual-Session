4a69d7318e6cf7e7667c4e492e4fc24ac197a377
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-04-18 01:24:18 +0600

    Update 2_Array_Input_Output.c

--- Batch-3/17-4-2023(Array)/2_Array_Input_Output.c
+++ Batch-3/17-4-2023(Array)/2_Array_Input_Output.c
@@ -7,21 +7,37 @@
 #include <stdio.h>
 int main()
 {
-    int arr[3];
+    int n;
+    scanf("%d",&n);
+    int arr[n];
 
-    // scanf("%d", &arr[0]);
-    // scanf("%d", &arr[1]);
-    // scanf("%d", &arr[2]);
-    for(int i=0;i<3;i++){
-        scanf("%d", &arr[i]);
+    // arr[0] = 1;
+    // arr[1] = 2;
+    // arr[2] = 3;
+    // arr[3] = 4;
+    // arr[4] = 5;
+
+    // scanf("%d",&arr[0]);
+    // scanf("%d",&arr[1]);
+    // scanf("%d",&arr[2]);
+    // scanf("%d",&arr[3]);
+    // scanf("%d",&arr[4]);
+
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-
 
     // printf("%d ",arr[0]);
     // printf("%d ",arr[1]);
     // printf("%d ",arr[2]);
-      for(int i=0;i<3;i++){
+    // printf("%d ",arr[3]);
+    // printf("%d ",arr[4]);
+
+    for(int i=0;i<n;i++){
         printf("%d ",arr[i]);
     }
+
+
+   
     return 0;
 }
