f343c5ab4dc2d64167bf8fc397f330c01c0566d6
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-04-17 16:28:25 +0600

    Create Ex_2_Swap.c

---
+++ Batch-3/17-4-2023(Array)/Ex_2_Swap.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+  int n;
+  scanf("%d",&n);
+  int arr[n];
+  for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+  }
+
+  int q;
+  scanf("%d",&q);
+  for(int i=1;i<=q;i++){
+    int l,r;
+    scanf("%d %d",&l,&r);
+    int temp;
+    temp = arr[l];
+    arr[l] = arr[r];
+    arr[r] = temp;
+  }      
+
+  for(int i=0;i<n;i++){
+    printf("%d ",arr[i]);
+  }  
+  return 0;
+}
