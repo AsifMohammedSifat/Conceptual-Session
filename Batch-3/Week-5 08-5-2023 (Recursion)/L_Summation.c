ae793a7fd156f5d86f966ac6919c636a2388b787
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-05-09 00:17:15 +0600

    Update L_Summation.c

--- Batch-3/Week-5 08-5-2023 (Recursion)/L_Summation.c
+++ Batch-3/Week-5 08-5-2023 (Recursion)/L_Summation.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-int solve(int arr[],int size){
+long long int solve(int arr[],int size){
+    //if(size<0) return 0;
     if(size==0) return arr[0];
     return arr[size] + solve(arr,size-1);
 }
@@ -10,7 +11,7 @@ int main(){
     for(int i=0;i<sz;i++){
         scanf("%d",&arr[i]);
     }
-    printf("%d\n",solve(arr,sz-1));        
+    printf("%lld\n",solve(arr,sz-1));        
               
     return 0;
 }
