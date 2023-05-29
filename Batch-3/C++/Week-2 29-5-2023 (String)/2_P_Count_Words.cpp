5c40e4c473a090192d7c2e430a19b0fb4c0f401f
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-05-29 18:57:22 +0600

    Update 2_P_Count_Words.cpp

--- Batch-3/C++/Week-2 29-5-2023 (String)/2_P_Count_Words.cpp
+++ Batch-3/C++/Week-2 29-5-2023 (String)/2_P_Count_Words.cpp
@@ -15,7 +15,7 @@ int main(){
     getline(cin,str);
 
     int count = 1;
-    for(int i=1;i<str.size();i++){
+    for(int i=1;i<str.size()-1;i++){
         if(!(str[i]>='A'&& str[i]<='z') && (str[i+1])>='A'&& str[i+1]<='z'){
             count++;
         }
