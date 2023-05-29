57237c67ec54494ac417af9e675c4ae8b40545f8
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-05-29 18:35:33 +0600

    Create 2_P_Count_Words.cpp

---
+++ Batch-3/C++/Week-2 29-5-2023 (String)/2_P_Count_Words.cpp
@@ -0,0 +1,30 @@
+/*
+||-----------------------||
+||  Asif Mohammed Sifat  ||
+||  Department of CSE    ||
+||   CS Instructor       ||
+||Phitron,ProgrammingHero||
+||-----------------------||
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    string str;
+    getline(cin,str);
+
+    int count = 1;
+    for(int i=1;i<str.size();i++){
+        if(!(str[i]>='A'&& str[i]<='z') && (str[i+1])>='A'&& str[i+1]<='z'){
+            count++;
+        }
+        else{
+            continue;
+        }
+    }
+
+    cout<<count<<endl;
+              
+    return 0;
+}
