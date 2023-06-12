2f71e99ce4b11814aee60757969a45a69788e9a4
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-06-13 01:58:29 +0600

    Update 4_deletion.cpp

--- Batch-3/DS/Week-2-1 12-06-2023 Singly Liked List/4_deletion.cpp
+++ Batch-3/DS/Week-2-1 12-06-2023 Singly Liked List/4_deletion.cpp
@@ -6,15 +6,6 @@
 ||Phitron,ProgrammingHero||
 ||-----------------------||
 */
-
-/***
- * Insert at Head
- * Print List
- * Insert at Tail
- * Count LikedList Size
- * Insert any index
- * 
-*/
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -23,83 +14,136 @@ class Node{
         int val;
         Node* next;
 
-        Node(int val){
-            this->val = val;
-            this->next = NULL;
-        }
+    Node(int val){
+       this->val = val;
+       this->next = NULL;        
+    }
+
 };
 
 int getSize(Node* head){
-    int count=0;
-
-    while(head!=NULL){
-        head = head->next;
+    Node* temp = head;
+    int count = 0;
+    while(temp!=NULL){
+        temp = temp->next;
         count++;
     }
     return count;
 }
 
-//insert at tail
+//insert at tail build linked list
 void insertAtTail(Node* &head,int val){
     Node* newNode = new Node(val);
     if(head==NULL){
         head = newNode;
         return;
     }
+
     Node* temp = head;
+
     while(temp->next!=NULL){
         temp = temp->next;
     }
     temp->next = newNode;
+
 }
+
+//Insert at head
+void insertAtHead(Node* &head,int val){
+    Node* newNode = new Node(val);
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
+
+    Node* temp = head;
+    head = newNode;
+    newNode->next = temp;
+
+}
+
+void insertAtAnyPosition(Node* &head,int index,int val){
+    Node* newNode = new Node(val);
+    if(index==1){
+        insertAtHead(head,val);
+        return;
+    }
+    else if(index==getSize(head)){
+        insertAtTail(head,val);
+        return;
+    }
+
+    Node* temp = head;
+
+    int pos = 1;
+
+    while(pos!=index-1){
+        temp = temp->next;
+        pos++;
+    }
+
+    Node* temp2 = temp->next;
+    temp->next = newNode;
+    newNode->next = temp2;
+
+}
+
+//delete at head
 void deleteFromHead(Node* &head){
     Node* temp = head;
-    Node* headNext = head->next;
-    head = headNext;
+
+    Node* temp2 = temp->next;
+    head = temp2;
     delete temp;
 }
 
+//delete from tail
 void deleteFromTail(Node* &head){
     Node* temp = head;
+
     while(temp->next->next!=NULL){
         temp = temp->next;
     }
-    Node* temp1 = temp->next;
+    Node* temp2 = temp->next;
+
     temp->next = NULL;
-    delete temp1;
+
+    delete temp2;
+
 }
 
-//print singly linked list
 void printList(Node* head){
     Node* temp = head;
-
+    
     while(temp!=NULL){
         cout<<temp->val<<" ";
         temp = temp->next;
     }
     cout<<endl;
+
 }
 
-
-
 int main(){
-
     Node* head = NULL;
     insertAtTail(head,10);
     insertAtTail(head,20);
-    insertAtTail(head,30);
-    printList(head);
+    insertAtTail(head,30);   
+    insertAtTail(head,30);   
+    insertAtTail(head,30);   
+    insertAtTail(head,30);   
+    insertAtTail(head,30);   
+    insertAtTail(head,30);   
+    insertAtTail(head,30);   
+    insertAtTail(head,30);   
+    insertAtTail(head,30);   
+    insertAtTail(head,30);   
+    insertAtTail(head,30);   
+    insertAtTail(head,30);   
+    insertAtTail(head,40);   
 
     deleteFromHead(head);
-    printList(head);
-
     deleteFromTail(head);
     printList(head);
-
-
-   
-
-
 
               
     return 0;
