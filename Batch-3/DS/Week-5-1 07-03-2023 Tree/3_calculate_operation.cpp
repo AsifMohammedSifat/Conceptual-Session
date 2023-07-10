df7d539c43699a76b920b0b285061a46dec21c8b
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-07-10 21:09:22 +0600

    Create 3_calculate_operation.cpp

---
+++ Batch-3/DS/Week-5-1 07-03-2023 Tree/3_calculate_operation.cpp
@@ -0,0 +1,130 @@
+/*
+||-----------------------||
+||  All Praise to Allah  ||
+||-----------------------||
+||  Asif Mohammed Sifat  ||
+||  Department of CSE    ||
+||   CS Instructor       ||
+||Phitron,ProgrammingHero||
+||-----------------------||
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+
+};
+Node* inputBinaryTree() {
+    int val;
+    cin >> val;
+    Node* root;
+    if (val == -1)
+        return NULL;
+    root = new Node(val);
+
+    queue<Node*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        Node* f = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+
+        if (l != -1) {
+            f->left = new Node(l);
+            q.push(f->left);
+        }
+
+        if (r != -1) {
+            f->right = new Node(r);
+            q.push(f->right);
+        }
+    }
+    return root;
+}
+
+void level_order(Node *root)
+{
+    if (root == NULL)
+    {
+        cout << "Tree nai" << endl;
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        // 1. ber kore ana
+        Node *f = q.front();
+        q.pop();
+
+        // 2. jabotiyo ja kaj ache
+        cout << f->val << " ";
+
+        // 3. tar children gulo ke rakha
+        if (f->left)
+            q.push(f->left);
+        if (f->right)
+            q.push(f->right);
+    }
+}
+
+//count number of node
+int count(Node *root)
+{
+    if (root == NULL)
+        return 0;
+    int l = count(root->left);
+    int r = count(root->right);
+    return l + r + 1;
+}
+
+//max-height
+int maxHeight(Node *root)
+{
+    if (root == NULL)
+        return 0;
+    int l = maxHeight(root->left);
+    int r = maxHeight(root->right);
+    return max(l, r) + 1;
+}
+
+//count leaf
+int count_leaf(Node *root)
+{
+    if (root == NULL)
+        return 0;
+    if (root->left == NULL && root->right == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        int l = count_leaf(root->left);
+        int r = count_leaf(root->right);
+        return l + r;
+    }
+}
+
+/*
+Input: 1 2 3 -1 4 5 -1 -1 -1 -1 -1
+Output: 1 2 3 4 5
+*/
+int main(){
+
+    Node* root = inputBinaryTree();
+    level_order(root);              
+    return 0;
+}
