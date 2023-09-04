0ca3f2369105e6db6207a7a35bdff4e3b029846e
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-09-04 18:08:30 +0600

    Create 1_cimbStairs.cpp

---
+++ Batch-3/Algorithm/Week-6-1 29-8-2023 Dp Introduction/1_cimbStairs.cpp
@@ -0,0 +1,68 @@
+/*
+||-----------------------||
+||  All Praise to Allah  ||
+||-----------------------||
+||  Asif Mohammed Sifat  ||
+||  Department of CSE    ||
+||   CS Instructor       ||
+||Phitron,ProgrammingHero||
+||-----------------------||
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+// General Approach
+class Solution {
+public:
+    
+    int solve(int n){        
+        if(n<=2) return n;
+        return solve(n-1)+solve(n-2); 
+    }
+    int climbStairs(int n) {
+        return solve(n);       
+    }
+};
+
+// Memoization
+class Solution {
+public:
+    
+    int solve(int n,vector<int>&dp){        
+        if(n<=2) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = solve(n-1,dp)+solve(n-2,dp); 
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);   
+        return solve(n,dp);       
+    }
+};
+
+//tabulation
+class Solution {
+public:
+    
+    int solve(int n,vector<int>&dp){      
+        if(n<=2) return n;  
+        
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+    int climbStairs(int n) {
+        vector<int>dp(n+1);   
+        return solve(n,dp);       
+    }
+};
+
+int main(){
+            
+              
+    return 0;
+}
