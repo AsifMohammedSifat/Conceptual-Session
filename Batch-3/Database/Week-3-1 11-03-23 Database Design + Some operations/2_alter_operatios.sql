774d74667650c51d81ddb1f935dc1b0f692ef438
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-12-05 20:21:31 +0600

    Create 2_alter_operatios.sql

---
+++ Batch-3/Database/Week-3-1 11-03-23 Database Design + Some operations/2_alter_operatios.sql
@@ -0,0 +1,67 @@
+==================================================================================================
+Add a New Column:
+
+                        ALTER TABLE table_name
+                        ADD COLUMN new_column_name datatype;
+                        Modify Column Data Type:
+
+==================================================================================================
+Modify Column Data Type:                    
+                        ALTER TABLE table_name
+                        MODIFY COLUMN existing_column_name new_datatype;
+                        Drop a Column:
+
+==================================================================================================
+Drop a Column:
+                          
+ALTER TABLE table_name
+DROP COLUMN column_name;
+Rename a Table:
+
+==================================================================================================
+Rename a Table:
+  
+ALTER TABLE old_table_name
+RENAME TO new_table_name;
+Change the Default Value of a Column:
+
+==================================================================================================
+Change the Default Value of a Column:
+  
+ALTER TABLE table_name
+ALTER COLUMN column_name SET DEFAULT new_default_value;
+Add a Primary Key:
+
+==================================================================================================
+Add a Primary Key:
+
+ALTER TABLE table_name
+ADD PRIMARY KEY (column_name);
+Add a Foreign Key:
+
+==================================================================================================
+Add a Foreign Key:
+  
+ALTER TABLE table_name
+ADD FOREIGN KEY (column_name) REFERENCES other_table(other_column);
+
+==================================================================================================
+Modify Indexes:
+  
+ALTER TABLE table_name
+ADD INDEX index_name (column_name);
+Modify Table Engine:
+
+==================================================================================================
+Modify Table Engine:
+  
+ALTER TABLE table_name
+ENGINE = InnoDB;
+Change Auto-Increment Seed and Increment Values:
+==================================================================================================
+Change Auto-Increment Seed and Increment Values:
+  
+ALTER TABLE table_name
+AUTO_INCREMENT = new_seed_value, AUTO_INCREMENT_INCREMENT = new_increment_value;
+
+==================================================================================================
