4fcd0525679179f7901a52f5f7d5bd34903f68f8
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-12-23 20:15:02 +0600

    Update 2_heirarchical_inheritance.py

--- Batch-2/29-3-2023(Graph)/Live Class/oop revision/2_heirarchical_inheritance.py
+++ Batch-2/29-3-2023(Graph)/Live Class/oop revision/2_heirarchical_inheritance.py
@@ -4,16 +4,32 @@ class Animal:
     def __init__(self,name) -> None:
         self.name = name
     def eat(self):
-        print(self.name," can eat")
+        print(f"{self.name} can eat")
 
 class Cat(Animal):
+    def __init__(self,name) -> None:
+        # super().__init__(name)
+        Animal.__init__(self,name)
+    # def eat(self):
+    #     print(f"{self.name} can eat")
     def sound(self):
-        print(self.name," sounds like meow")
+        print(self.name," sounds like Meeow!")
+    
+    def __repr__(self) -> str:
+        return f"{self.name} class"
 
 class Dog(Animal):
+    # def __init__(self,name) -> None:
+    #     self.name = name
+    # def eat(self):
+    #     print(f"{self.name} can eat")
     def sound(self):
-        print(self.name," sounds like bark")
+        print(self.name," sounds like Bark!")
 
-cat = Cat("Cat")
-cat.sound()
 
+c = Cat("Cat")
+c.eat()
+d = Dog("Dog")
+d.eat()
+
+print(c)
