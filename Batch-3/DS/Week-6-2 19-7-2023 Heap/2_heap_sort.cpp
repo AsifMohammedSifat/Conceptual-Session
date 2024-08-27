9282b62f3b8c5756d9b17c433897464e538fb682
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2024-08-27 22:02:44 +0600

    Update 2_heap_sort.cpp

--- Batch-3/DS/Week-6-2 19-7-2023 Heap/2_heap_sort.cpp
+++ Batch-3/DS/Week-6-2 19-7-2023 Heap/2_heap_sort.cpp
@@ -1,139 +1,99 @@
-//from module
-#include <bits/stdc++.h>
-using namespace std;
+Problem LinK: https://leetcode.com/problems/sort-an-array/
 
-class MaxHeap{
+
+class MaxHeap
+{
 public:
-    vector<int>nodes;
-
+    vector<int> nodes;
     MaxHeap()
     {
-
     }
 
-    //O(logn)
+    // O(logn)
     void up_heapify(int idx)
     {
-        while(idx > 0 && nodes[idx] > nodes[(idx-1)/2])
+        while (idx > 0 && nodes[idx] > nodes[(idx - 1) / 2])
         {
-            swap(nodes[idx] , nodes[(idx-1)/2]);
-            idx = (idx-1)/2;
+            swap(nodes[idx], nodes[(idx - 1) / 2]);
+            idx = (idx - 1) / 2;
         }
     }
-    //O(logn)
-    void insert(int x)
+
+    void push(int val)
     {
-        nodes.push_back(x);
-        up_heapify(nodes.size()-1);
+        nodes.push_back(val);
+        up_heapify(nodes.size() - 1);
     }
 
-    //O(n)
-    void PrintHeap()
-    {
-        for(int i=0;i<nodes.size();i++)
-        {
-            cout<<nodes[i]<<" ";
-        }
-        cout<<"\n";
-    }
-
-    //O(logn)
     void down_heapify(int idx)
     {
-        while(1)
+        int largest = idx;
+        while (1)
         {
-            int largest = idx;
-            int l = 2*idx + 1;
-            int r = 2*idx + 2;
-            if(l<nodes.size() && nodes[largest] < nodes[l])
-            {
-                largest = l;
-            }
-            if(r<nodes.size() && nodes[largest] < nodes[r])
-            {
-                largest = r;
-            }
-            if(largest==idx)
+            int l = idx * 2 + 1;
+            int r = idx * 2 + 2;
+            if (l < nodes.size() && nodes[l] > nodes[largest])
+                largest = idx * 2 + 1;
+            if (r < nodes.size() && nodes[r] > nodes[largest])
+                largest = idx * 2 + 2;
+            if (idx == largest)
                 break;
-            swap(nodes[idx] , nodes[largest]);
+            swap(nodes[idx], nodes[largest]);
             idx = largest;
         }
-
     }
 
-    //O(logn)
-    void Delete(int idx)
+    void pop(int idx)
     {
-        if(idx >= nodes.size())
-            return;
-        swap(nodes[idx] , nodes[nodes.size()-1]);
+        swap(nodes[idx], nodes[nodes.size() - 1]);
         nodes.pop_back();
         down_heapify(idx);
     }
 
-    //O(1)
-    int getMax()
+    void buildHeapFromArray(vector<int> &arr)
     {
-        if(nodes.empty())
-        {
-            cout<<"Heap is empty!\n";
-            return -1;
-        }
-        return nodes[0];
-    }
+        nodes = arr;
+        int lastLeafNode = arr.size() / 2 - 1;
 
-    //O(logn)
-    int ExtractMax()
-    {
-        if(nodes.empty())
-        {
-            cout<<"Heap is empty!\n";
-            return -1;
-        }
-        int ret = nodes[0];
-        Delete(0);
-        return ret;
-    }
-
-    //O(n)
-    void build_from_array(vector<int>&a)
-    {
-        nodes = a;
-        int n = nodes.size();
-        int last_non_leaf = n/2 - 1;
-        for(int i=last_non_leaf; i>=0; i--)
+        for (int i = lastLeafNode; i >= 0; i--)
         {
             down_heapify(i);
         }
     }
+
+    int ExtractMax(){
+        int max = nodes[0];
+        pop(0);
+        return max;
+    }
+
+    void print()
+    {
+        for (int node : nodes)
+        {
+            cout << node << " ";
+        }
+        cout << endl;
+    }
 };
 
-//O(nlogn)
-vector<int> heap_sort(vector<int>a)
-{
-    MaxHeap h;
-    h.build_from_array(a);
-    vector<int>ans;
-    for(int i=0;i<a.size();i++)
-    {
-        ans.push_back(h.ExtractMax());
-    }
-    reverse(ans.begin(),ans.end());
-    return ans;
-}
-int main()
-{
-//    MaxHeap h;
-//    vector<int>a = {1,2,3,4,10,9,8,7};
-//    h.build_from_array(a);
-//    h.PrintHeap();
 
-    vector<int>a = {1,2,3,4,10,9,5,7};
-    vector<int>sorted_a = heap_sort(a);
-    for(int i=0;i<sorted_a.size();i++)
-    {
-        cout<<sorted_a[i]<<" ";
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+    MaxHeap heap;
+    heap.buildHeapFromArray(nums);
+
+    vector<int>result;
+    int len = nums.size();
+    for(int i=0;i<len;i++){
+        result.push_back(heap.ExtractMax());
     }
-    cout<<"\n";
-    return 0;
-}
+
+    reverse(result.begin(),result.end());
+
+    return result;
+    
+        
+    }
+};
